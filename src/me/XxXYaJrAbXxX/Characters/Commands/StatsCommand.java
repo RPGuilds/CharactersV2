1d61f405cfa9e09986e3c823b042472d65dbd95c
Jay Bartmeier <yajrab@gmail.com>
2016-01-10 17:17:17 +0000

    Maven update and stats command formatting

--- src/me/XxXYaJrAbXxX/Characters/Commands/StatsCommand.java
+++ src/me/XxXYaJrAbXxX/Characters/Commands/StatsCommand.java
@@ -23,9 +23,9 @@ public class StatsCommand {
 			playersname = sender.getName();
 		}
 		try {
-		String selected = GetData.get(playersname, "selected");
-		String characterRace = GetData.get(playersname, "character" + selected + ".race");
-		String characterClass = GetData.get(playersname, "character" + selected + ".class");
+			String selected = GetData.get(playersname, "selected");
+			String characterRace = GetData.get(playersname, "character" + selected + ".race");
+			String characterClass = GetData.get(playersname, "character" + selected + ".class");
 			int raceStrength = (int) GetData.getDefaultConfig(characterRace + "_Strength");
 			int raceDexterity = (int) GetData.getDefaultConfig(characterRace + "_Dexterity");
 			int raceWisdom = (int) GetData.getDefaultConfig(characterRace + "_Wisdom");
@@ -34,21 +34,22 @@ public class StatsCommand {
 			int classDexterity = (int) GetData.getDefaultConfig(characterClass + "_Dexterity");
 			int classWisdom = (int) GetData.getDefaultConfig(characterClass + "_Wisdom");
 			int classAgility = (int) GetData.getDefaultConfig(characterClass + "_Agility");
-		int Strength = raceStrength + classStrength;
-		int Dexterity = raceDexterity + classDexterity;
-		int Wisdom = raceWisdom + classWisdom;
-		int Agility = raceAgility + classAgility;
-		if (playersname == sender.getName()) {
-			sender.sendMessage(ChatColor.GOLD + "Your character's strength stat is: " + ChatColor.WHITE + Strength);
-			sender.sendMessage(ChatColor.GOLD + "Your character's ranged stat is: " + ChatColor.WHITE + Dexterity);
-			sender.sendMessage(ChatColor.GOLD + "Your character's magic stat is: " + ChatColor.WHITE + Wisdom);
-			sender.sendMessage(ChatColor.GOLD + "Your character's agility stat is: " + ChatColor.WHITE + Agility);
-		} else {
-			sender.sendMessage(ChatColor.GOLD + "Their character's strength stat is: " + ChatColor.WHITE + Strength);
-			sender.sendMessage(ChatColor.GOLD + "Their character's ranged stat is: " + ChatColor.WHITE + Dexterity);
-			sender.sendMessage(ChatColor.GOLD + "Their character's magic stat is: " + ChatColor.WHITE + Wisdom);
-			sender.sendMessage(ChatColor.GOLD + "Their character's agility stat is: " + ChatColor.WHITE + Agility);
-		}
+			int Strength = raceStrength + classStrength;
+			int Dexterity = raceDexterity + classDexterity;
+			int Wisdom = raceWisdom + classWisdom;
+			int Agility = raceAgility + classAgility;
+			if (playersname == sender.getName()) {
+				sender.sendMessage(ChatColor.GOLD + "Your character's strength stat is: " + ChatColor.WHITE + Strength);
+				sender.sendMessage(ChatColor.GOLD + "Your character's ranged stat is: " + ChatColor.WHITE + Dexterity);
+				sender.sendMessage(ChatColor.GOLD + "Your character's magic stat is: " + ChatColor.WHITE + Wisdom);
+				sender.sendMessage(ChatColor.GOLD + "Your character's agility stat is: " + ChatColor.WHITE + Agility);
+			} else {
+				sender.sendMessage(
+						ChatColor.GOLD + "Their character's strength stat is: " + ChatColor.WHITE + Strength);
+				sender.sendMessage(ChatColor.GOLD + "Their character's ranged stat is: " + ChatColor.WHITE + Dexterity);
+				sender.sendMessage(ChatColor.GOLD + "Their character's magic stat is: " + ChatColor.WHITE + Wisdom);
+				sender.sendMessage(ChatColor.GOLD + "Their character's agility stat is: " + ChatColor.WHITE + Agility);
+			}
 		} catch (Exception e) {
 			sender.sendMessage(ChatColor.RED + "You must set up your character sheet properly first!");
 		}
